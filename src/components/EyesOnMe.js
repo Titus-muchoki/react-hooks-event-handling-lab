57b7dbaa46dd07d8d965809e284242a511b88ad7
kajela <titoyut56@gmail.com>
2023-06-07 07:47:20 -0400

    add the initial files

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,8 +1,14 @@
 // Code EyesOnMe Component Here
 import React from "react"
 function EyesOnMe(){
+    function handlerFocus(){
+        console.log("Good!")
+    }
+    function handleBlur(){
+        console.log("Hey! Eyes on me!")
+    }
     return(
-        <button>Eyes on me</button>
+        <button onFocus={handlerFocus} onBlur={handleBlur}>Eyes on me</button>
     )
 }
 export default EyesOnMe;
